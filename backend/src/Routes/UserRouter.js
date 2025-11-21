669b2737b15da46eb7033a1f8c078e1bf95282d5
ndayiringiye <ndayiringiyedavid120@gmail.com>
2025-11-21 16:00:05 +0100

    feat:/student currently in progress functionailty

--- backend/src/Routes/UserRouter.js
+++ backend/src/Routes/UserRouter.js
@@ -2,9 +2,12 @@ import express from "express";
 import UserRegister from "../services/userService.js"; 
 import { AdminRegister} from "../services/AdminService.js";
 import { AdminLogin } from "../services/AdminService.js";
+import { studentLogin } from "../services/userService.js";
 const router = express.Router();
 
 router.post("/users/signup", UserRegister);
+router.post("/users/student/login", studentLogin);
+
 router.post("/users/admin/register", AdminRegister);
 router.post("/admin/login", AdminLogin);
 
